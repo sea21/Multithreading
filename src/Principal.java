286d743aa57059072e68ee0c98ca99c72737c5cf
Sara <saraespinal1@hotmail.com>
2015-11-21 23:49:52 -0500

    Programa funcional
    Faltan hilos

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,168 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.XMLWriter;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		//Scanner leer = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		Scanner leer = new Scanner(System.in);
 		System.out.println("Ingrese el número de comprobantes: ");
-		//int nroComprobantes = leer.nextInt();
+		int nroComprobantes = leer.nextInt();
+		createDocument(nroComprobantes);
 		
-		GeneraFechas fecha = new GeneraFechas();
-		GeneraNumeros numero = new GeneraNumeros();
+	}
+	
+	public static Document createDocument(int nroComprobantes) throws IOException {
 		
 		ObtenerEmpresayTercero empresaytercero = new ObtenerEmpresayTercero();
+		List<String> codigosTerceros = empresaytercero.obtenerCodigos();
+		List<String> nombresTerceros = empresaytercero.obtenerNombres();
 		
 		ObtenerCuentas obtenerCuentas = new ObtenerCuentas();
+		List<String> codCuentas = obtenerCuentas.obtenerCodigos();
+		List<String> nomCuentas = obtenerCuentas.obtenerNombres();
 		
-		obtenerCuentas.obtenerNombres();
-	}
+		Document document = null;
+        for (int i=0; i<nroComprobantes; i++){
+        	
+        	document = DocumentHelper.createDocument();
+            
+            Element root = document.addElement( "Comprobante" );
+        	
+            GeneraFechas fecha = new GeneraFechas();
+    		GeneraNumeros numero = new GeneraNumeros();
+        	
+        	int emp = (int)Math.floor(Math.random()*(0-(codigosTerceros.size()+1))+(codigosTerceros.size()+1));
+        	
+        	int ter = (int)Math.floor(Math.random()*(0-(codigosTerceros.size()+1))+(codigosTerceros.size()+1));
+
+            Element tipo = root.addElement( "Tipo" )
+                .addText( "Comprobante de Egreso" );
+            
+            Element numeroCompe = root.addElement( "Numero" )
+                .addText( numero.numero() );
+
+            Element fechaComp = root.addElement( "Fecha" )
+                    .addText( fecha.fecha() );
+            
+            Element nota = root.addElement( "Nota" )
+                    .addText( "Ninguna." );
+            
+            Element empresa = root.addElement( "Empresa" );
+          
+            	Element codEmpresa = empresa.addElement("Codigo")
+            			.addText( codigosTerceros.get(emp).toString() );
+            
+            	Element nomEmpresa = empresa.addElement("Nombre")
+            			.addText( nombresTerceros.get(emp).toString() );
+            
+            Element tercero = root.addElement("Tercero");
+            
+            	Element codTercero = tercero.addElement("Codigo")
+            			.addText( codigosTerceros.get(ter).toString() );
+            
+            	Element nomTercero = tercero.addElement("Nombre")
+            			.addText( nombresTerceros.get(ter).toString() );
+            
+            Element totales = root.addElement("Totales");
+            
+            	Element valor = totales.addElement("Valor")
+            			.addText( "" );
+            
+            Element conta = root.addElement("Contabilizacion");
+
+            int nroAsientos = numero.asientos();
+            
+            for (int j = 0; j < nroAsientos; j++) {
+            
+            GeneraDebitosCreditos genDebyCred = new GeneraDebitosCreditos();
+            int debito = genDebyCred.debito();
+            int credito = genDebyCred.credito(debito);
+            
+            // **** Asiento 1 ****
+            
+            int cuenta1 = (int)Math.floor(Math.random()*
+            		(0-(codCuentas.size()+1))+(codCuentas.size()+1));
+            
+            Element as1 = conta.addElement("Asiento");
+            
+            	Element libro = as1.addElement("Libro")
+            			.addText( "COMUN" );
+            
+            	Element cuenta = as1.addElement("Cuenta");
+            
+            		Element codCuenta = cuenta.addElement("Codigo")
+            				.addText( codCuentas.get(cuenta1).toString() );
+            
+            		Element nomCuenta = cuenta.addElement("Nombre")
+            				.addText( nomCuentas.get(cuenta1).toString() );
+            
+	            Element terceroAs1 = as1.addElement("Tercero");
+	            
+	            	Element codTerceroAs1 = terceroAs1.addElement("Codigo")
+	            			.addText( codigosTerceros.get(ter).toString() );
+	            
+	                Element nomTerceroAs1 = terceroAs1.addElement("Nombre")
+	                		.addText( nombresTerceros.get(ter).toString() );
+	                
+	            Element debitoAs1 = as1.addElement("Debitos")
+                		.addText( Integer.toString(debito) );
+                
+                Element creditoAs1 = as1.addElement("Creditos")
+                		.addText( "" );
+                
+            // **** Asiento 2 ****
+                
+            int cuen2 = (int)Math.floor(Math.random()*
+              		(0-(codCuentas.size()+1))+(codCuentas.size()+1));
+            
+            Element as2 = conta.addElement("Asiento");
+                
+            	Element libro2 = as2.addElement("Libro")
+            			.addText( "COMUN" );
+            
+            	Element cuenta2 = as2.addElement("Cuenta");
+            
+            		Element codCuenta2 = cuenta2.addElement("Codigo")
+            				.addText( codCuentas.get(cuen2).toString() );
+            
+            		Element nomCuenta2 = cuenta2.addElement("Nombre")
+            				.addText( nomCuentas.get(cuen2).toString() );
+            
+	            Element terceroAs2 = as2.addElement("Tercero");
+	            
+	            	Element codTerceroAs2 = terceroAs2.addElement("Codigo")
+	            			.addText( codigosTerceros.get(ter).toString() );
+	            
+	                Element nomTerceroAs2 = terceroAs2.addElement("Nombre")
+	                		.addText( nombresTerceros.get(ter).toString() );
+	                
+	            Element debitoAs2 = as2.addElement("Debitos")
+                		.addText( "" );
+                
+                Element creditoAs2 = as2.addElement("Creditos")
+                		.addText( Integer.toString(credito) );
+            
+            }
+            // lets write to a file
+            XMLWriter writer = new XMLWriter(
+                new FileWriter( "salidas/comprobante"+ (i+1) + ".xml" )
+            );
+            writer.write( document );
+            writer.close();
+           
+        }
+        System.out.println("Comprobantes creados.");
+        System.out.println("Tiempo : ");
+        return document;
+    }
 
 }
