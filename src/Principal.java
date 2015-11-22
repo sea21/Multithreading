dd6803f6c92d789c3c976b40867e5122a8bc76dd
Sara <saraespinal1@hotmail.com>
2015-11-21 20:11:29 -0500

    Proyecto creado
    Ingresar número de comprobantes

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Ingrese el número de comprobantes: ");
+		int nroComprobantes = leer.nextInt();
+
+	}
+
+}
