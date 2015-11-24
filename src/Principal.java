b4d15abc8cb9f0fa12d26adf28076b1b82c7f93a
Sara <saraespinal1@hotmail.com>
2015-11-24 15:49:33 -0500

    Commit final

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,5 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
-
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
-import org.dom4j.io.XMLWriter;
 
 public class Principal extends Thread{
 	
@@ -20,34 +10,64 @@ public class Principal extends Thread{
 		
 		int nroComprobantes = leer.nextInt();
 		
-		int mitad = nroComprobantes / 4;
-		
-		CrearDocumento crearDocu = new CrearDocumento();
-		
-		CrearDocumentosThread t1, t2, t3, t4;
-		t1 = new CrearDocumentosThread(crearDocu, mitad);
-		t2 = new CrearDocumentosThread(crearDocu, mitad);
-		t3 = new CrearDocumentosThread(crearDocu, mitad);
-		t4 = new CrearDocumentosThread(crearDocu, mitad);
-		
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		
-		t1.join();
-		t2.join();
-		t3.join();
-		t4.join();
-		
+		if (nroComprobantes == 1){
+			
+			CrearDocumento crearDocu = new CrearDocumento();
+			CrearDocumentosThread t1;
+			t1 = new CrearDocumentosThread(crearDocu, nroComprobantes);
+			t1.start();
+			t1.join();
+			
+		}else if((nroComprobantes % 2) == 0 ){
+			
+				int mitad = nroComprobantes / 2;
+				CrearDocumento crearDocu = new CrearDocumento();
+				CrearDocumentosThread t1, t2;
+				t1 = new CrearDocumentosThread(crearDocu, mitad);
+				t2 = new CrearDocumentosThread(crearDocu, mitad);
+				t1.start();
+				t2.start();
+				t1.join();
+				t2.join();
+				
+			}else if ((nroComprobantes % 3) == 0) {
+				
+				int mitad = nroComprobantes / 3;
+				CrearDocumento crearDocu = new CrearDocumento();
+				CrearDocumentosThread t1, t2, t3;
+				t1 = new CrearDocumentosThread(crearDocu, mitad);
+				t2 = new CrearDocumentosThread(crearDocu, mitad);
+				t3 = new CrearDocumentosThread(crearDocu, mitad);
+				t1.start();
+				t2.start();
+				t3.start();
+				t1.join();
+				t2.join();
+				t3.join();
+				
+			}else {
+				int mitad = nroComprobantes / 4;
+				CrearDocumento crearDocu = new CrearDocumento();
+				CrearDocumentosThread t1, t2, t3, t4;
+				t1 = new CrearDocumentosThread(crearDocu, mitad);
+				t2 = new CrearDocumentosThread(crearDocu, mitad);
+				t3 = new CrearDocumentosThread(crearDocu, mitad);
+				t4 = new CrearDocumentosThread(crearDocu, mitad);
+				t1.start();
+				t2.start();
+				t3.start();
+				t4.start();
+				t1.join();
+				t2.join();
+				t3.join();
+				t4.join();
+			}
+			
 		Date horaFin = new Date();
 		
 		Long tiempo = horaFin.getTime() - horaInicio.getTime();
 		
 		System.out.println("Comprobantes creados.");
 		System.out.println("Tiempo total : " + tiempo.doubleValue()/1000 + " segundos" );
-		
+		}
 	}
-	
-
-}
